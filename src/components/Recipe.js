b388ed348f41ead004b834ddde3107bdddd1e274
mccarthy417nahant <kevinmccarthy01@comcast.net>
2018-12-12 18:47:11 -0500

    finished but broken

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,7 +1,43 @@
 import React from 'react';
 
-const Recipe = () => (
-  <div>Recipe Component!</div>
-);
+import { Link } from "react-router-dom";
+
+const API_KEY = "bc9e815c4a262286447098327ef72b95";
+
+class Recipe extends React.Component {
+  state = {
+    activeRecipe: []
+  }
+  componentDidMount = async () => {
+    const title = this.props.location.state.recipe;
+    const req = await fetch
+    (`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${title}`);
+
+    const res = await req.json();
+    this.setState({activeRecipe: res.recipes[0]});
+  }
+  render() {
+    const recipe = this.state.activeRecipe;
+    return (
+      <div className="container">
+        { this.state.activeRecipe.length !== 0 &&
+          <div className="active-recipe">
+            <img className="active-recipe__img" src={recipe.image_url} alt={recipe.title}/>
+            <h3 className="active-recipe__title">{recipe.title}</h3>
+            <h4 className="active-recipe__publisher">
+              Publisher: <span>{recipe.publisher}</span>
+            </h4>
+            <p className="active-recipe__recipe">Recipe:
+              <span><a href={recipe.source_url}>{recipe.source_url}</a></span>
+            </p>
+            <button className="active-recipe__button">
+              <Link to="/" >Go Home</Link>
+            </button>
+          </div>
+        }
+      </div>
+    );
+  }
+}
 
 export default Recipe;
