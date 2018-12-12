6b1642a7ab61d3aee9c68309ada0634d75b9b181
mccarthy417nahant <kevinmccarthy01@comcast.net>
2018-12-12 15:31:02 -0500

    initial commit somewhat functioning recipe search

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+import Form from "./components/Form";
+import Recipes from "./components/Recipes";
+
+const API_KEY = "bc9e815c4a262286447098327ef72b95";
+
 class App extends Component {
+  state ={
+    recipes:[]
+  }
+  getRecipe = async (e) => {
+    const recipeName = e.target.elements.recipeName.value;
+    e.preventDefault();
+    const api_call = await fetch(`https://cors-anywhere.herokuapp.com/https://www.food2fork.com/api/search?key=${API_KEY}&q=${recipeName}&count=10`);
+
+    const data = await api_call.json();
+    this.setState({ recipes: data.recipes});
+    console.log(this.state.recipes)
+  }
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <h1 className="App-title">Recipe Search</h1>
         </header>
+        <Form getRecipe={this.getRecipe}/>
+        <Recipes recipes={this.state.recipes}/>
       </div>
     );
   }
