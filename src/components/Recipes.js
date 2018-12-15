1bf3fdb110d7198a958f40078ad99b8c8a83be93
mccarthy417nahant <kevinmccarthy01@comcast.net>
2018-12-15 16:05:30 -0500

    fix for heroku

--- src/components/Recipes.js
+++ src/components/Recipes.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {Link} from "react-router-dom";
 
-const Recipes = props => (
+const Recipes = ({recipes = []}) => (
   <div className="container">
     <div className="row">
     { props.recipes.map((recipe) => {
